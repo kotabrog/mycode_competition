9e0b14d84662d8964df83e0cada66d0c6e8818db
kota <suzuki.kota0331@gmail.com>
2020-04-12 22:42:13 +0900

    20/04/12 2

---
+++ AtCoder_Problems/ABC/real_time/162/A/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int N;
+
+  cin >> N;
+  if (N / 100 == 7 || (N / 10) % 10 == 7 || N % 10 == 7)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+}
