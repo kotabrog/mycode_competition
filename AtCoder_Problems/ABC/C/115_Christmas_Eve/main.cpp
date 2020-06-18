40aba42d0109928cad6092537d332899399747fd
kota <suzuki.kota0331@gmail.com>
2020-06-18 16:25:21 +0900

    20/06/18 2

--- AtCoder_Problems/ABC/C/115_Christmas_Eve/main.cpp
+++ AtCoder_Problems/ABC/C/115_Christmas_Eve/main.cpp
@@ -9,5 +9,14 @@ int lcm(int a,int b){return a*b/gcd(a, b);}
 
 int main()
 {
-  
+  int n, k, ans = 1000000000;
+  vector<int> h(100000);
+
+  cin >> n >> k;
+  for (int i = 0; i < n; i++)
+    cin >> h[i];
+  sort(h.begin(), h.begin() + n);
+  for (int i = 0; i < n - k + 1; i++)
+    ans = min(ans, h[i + k - 1] - h[i]);
+  cout << ans << endl;
 }
