d6e6f35a217fa3fe7767502defe01441361b944f
kota <suzuki.kota0331@gmail.com>
2020-08-06 00:03:46 +0900

    20/08/05 first commit

---
+++ Codingame/Ghost_Legs/test.py
@@ -0,0 +1,20 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+w, h = [int(i) for i in input().split()]
+for i in range(h):
+	line = input()
+
+	if i == 0:
+		top = []
+		for c in line:
+			
+
+
+
+print("answer")
