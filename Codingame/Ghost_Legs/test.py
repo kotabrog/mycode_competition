eace6611a5d5e8a2785ba7476a5c83fa628f4dc3
kota <suzuki.kota0331@gmail.com>
2020-08-06 17:33:02 +0900

    finish ghost legs

--- Codingame/Ghost_Legs/test.py
+++ Codingame/Ghost_Legs/test.py
@@ -8,13 +8,32 @@ import math
 
 w, h = [int(i) for i in input().split()]
 for i in range(h):
-	line = input()
+	l = input()
+	line = [c for c in l]
 
 	if i == 0:
-		top = []
-		for c in line:
-			
+		dic = {}
+		row_dic = {}
+		dic_len = 0
+		for j in range(w):
+			if line[j] != ' ':
+				dic[dic_len] = line[j]
+				row_dic[j] = line[j]
+				dic_len += 1
 
+	elif i != h - 1:
+		h_temp = 0
+		for j in range(1, w):
+			if j <= h_temp:
+				continue
+			if line[j] == '-':
+				l_temp = max([k for k in row_dic.keys() if k < j])
+				h_temp = min([k for k in row_dic.keys() if j < k])
+				row_dic[l_temp], row_dic[h_temp] = row_dic[h_temp], row_dic[l_temp]
 
-
-print("answer")
+	else:
+		row_dic_reverse = {v: k for k, v in row_dic.items()}
+		print("row_dic:", row_dic, file=sys.stderr, flush=True)
+		print("dic", dic, file=sys.stderr, flush=True)
+		for j in range(dic_len):
+			print(dic[j], line[row_dic_reverse[dic[j]]], sep='')
